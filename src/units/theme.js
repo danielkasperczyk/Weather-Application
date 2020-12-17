dc715c5748293ca02c0b974af3c877e60a255b8a
danielkasperczyk <75161634+danielkasperczyk@users.noreply.github.com>
2020-12-17 20:28:34 +0100

    added colors for search component

--- src/units/theme.js
+++ src/units/theme.js
@@ -3,18 +3,23 @@ const colors = {
     light: '#EEEFF7',
     purple: '#445878',
     green: '#92CDCF',
+    searchDark: '#454852',
+    searchLight: '#c4c5cc'
+
 }
 
 export const lightTheme = {
     bg: colors.light,
     box: colors.purple,
     add: colors.green,
-    text: colors.dark
+    text: colors.dark,
+    search: colors.searchLight
 }
 
 export const darkTheme = {
     bg: colors.dark,
     box: colors.purple,
     add: colors.green,
-    text: colors.light
+    text: colors.light,
+    search: colors.searchDark
 }
