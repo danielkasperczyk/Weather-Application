a91afcb549b19be9b6acfb51cfb7865fba865422
danielkasperczyk <75161634+danielkasperczyk@users.noreply.github.com>
2020-12-17 20:29:10 +0100

    changing first letter in input to uppercase

--- src/units/helpers.js
+++ src/units/helpers.js
@@ -2,9 +2,7 @@ import axios from 'axios';
 
 const API_KEY = '82339489472861515d0a365415708a6e';
 
-// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-
-const DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+// const DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 export async function getCityName(lat, lon){
     const response = await axios.get(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lon}&localityLanguage=pl`)
@@ -22,7 +20,19 @@ export async function getWeatherByName(city){
 }
 
 export async function reverseGeolocation(city){
-    const response = await axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${city}&key=85e2f7977b8743ae855a296daa1f9d29`);
-    const {lat,lng} = await response.data.results[0].geometry;
-    return {lat, lng};
+    const response = await axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${city}&key=85e2f7977b8743ae855a296daa1f9d29`)
+    const result = response.data.results
+    if(result.length > 0) {
+        return result
+    }
+    else {
+        return new Error('City not found');
+    }
+    // const {lat,lng} = await response.data.results[0].geometry;
+    // return {lat, lng};
 }
+
+export function transformToUpperCase(string){
+    return string.split(' ').map(str => str.charAt(0).toUpperCase() + str.substr(1)).join(' ');
+}
+
